299004efbf5743998b921d294b581ff539145c2e
cesarhernandezgt <cfhernan@sat.gob.gt>
2013-07-15 17:02:48 -0600

    Se agrego null poiniter dereference demo.

--- demo1/src/main/java/otn/ouggt/org/demo1/App.java
+++ demo1/src/main/java/otn/ouggt/org/demo1/App.java
@@ -1,13 +1,35 @@
 package otn.ouggt.org.demo1;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	/* NULL POINTER DEREFERENCE
+	 * C—digo de ECLIPSE 3.5RC3
+	 * org.eclipse.update.internal.ui.views.FeatureStateAtion 
+	 */
+	@SuppressWarnings("null")
+	public int returnSomething(List<?> adapters){
+		if (adapters == null  && adapters.size() ==0){
+			//do something
+			return 1;
+		}else{
+			//do something else
+			return 0;
+		}		
+	}
+
+
+	public static void main (String args[]){
+		App obj = new App();
+		int i = obj.returnSomething(null);
+		System.out.println(i);
+	}
+    
+    
+    
 }
