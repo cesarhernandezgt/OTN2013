a7c1d0128d7708e5c1874d9cf22aa905cf0c029a
cesarhernandezgt <cfhernan@sat.gob.gt>
2013-07-15 14:39:15 -0600

    Commit inicial de proyecto Demo1

---
+++ demo1/src/main/java/otn/ouggt/org/demo1/App.java
@@ -0,0 +1,13 @@
+package otn.ouggt.org.demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
